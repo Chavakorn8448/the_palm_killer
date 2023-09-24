87ed670ea3f76ee21e72a5b31d23fb1a51cccff8
Chavakorn8448 <arunkunarax.c.8448@gmail.com>
2023-09-24 16:49:21 +0700

    9/24

--- image.py
+++ image.py
@@ -1,6 +1,20 @@
 import evdev
 from evdev import ecodes
 import os
+from PIL import Image
+
+def create_image(number):
+    # Image size
+    width, height = 300, 300
+
+    # Color: in this case, it's red. You can choose any RGB value.
+    color = (255, 0, 0)
+
+    # Create an image with the given color
+    image = Image.new('RGB', (width, height), color)
+
+    # Save the image as a .jpg file
+    image.save(number, '.jpg')
 
 # get last file number
 def get_only_filename(files):
@@ -47,7 +61,6 @@ keyboard = keyboard_devices[2]
 print(f"\nListening for key press on device: {keyboard.name}")
 
 directory = os.path.abspath("/home/traffy/the_palm_killer")
-get_highest_numbered_file(directory)
 
 unripe = os.path.join(directory, 'unripe')
 almost_ripe = os.path.join(directory, 'almost_ripe')
@@ -83,26 +96,40 @@ if not os.path.exists(too_ripe):
         with open('too_ripe.txt', 'w') as f:
             pass
 
+unripe_last_num = get_highest_numbered_file(unripe)
+almost_ripe_last_num = get_highest_numbered_file(almost_ripe)
+ripe_last_num = get_highest_numbered_file(ripe)
+too_ripe_last_num = get_highest_numbered_file(too_ripe)
+
+if unripe_last_num is None:
+    unripe_last_num = 1
+if almost_ripe_last_num is None:
+    almost_ripe_last_num = 1
+if ripe_last_num is None:
+    ripe_last_num = 1
+if too_ripe_last_num is None:
+    too_ripe_last_num = 1
+
 # Infinite loop to check for key events
 for event in keyboard.read_loop():
 # Check if a key is pressed down and if it's the '1' key
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_1:
         print("'1' key pressed!")
         os.chdir(unripe)
-        with open('unripe.txt', 'a') as f:
-            f.write('1\n') # write the data recieve
+        create_image(unripe_last_num)
+        unripe_last_num += 1
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_2:
         print("'2' key pressed!")
         os.chdir(almost_ripe)
-        with open('almost_ripe.txt', 'a') as f:
-            f.write('2\n') # write the data recieve
+        create_image(almost_ripe_last_num)
+        almost_ripe_last_num += 1
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_3:
         print("'3' key pressed!")
         os.chdir(ripe)
-        with open('ripe.txt', 'a') as f:
-            f.write('3\n') # write the data recieve
+        create_image(ripe_last_num)
+        ripe_last_num += 1
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_4:
         print("'4' key pressed!")
         os.chdir(too_ripe)
-        with open('too_ripe.txt', 'a') as f:
-            f.write('4\n') # write the data recieve
+        create_image(too_ripe_last_num)
+        too_ripe_last_num += 1
--- test.py
+++ test.py
@@ -1,34 +1,13 @@
-import os
+from PIL import Image
 
-def get_only_filename(files):
-    lst_file = []
-    for file in files:
-        lst_file.append(file.split('.')[0])
-    return lst_file
+# Image size
+width, height = 300, 300
 
-def get_highest_numbered_file(path):
-    try:
-        # List all files in the directory
-        files = os.listdir(path)
-        print(files)
-        new_files = get_only_filename(files)
+# Color: in this case, it's red. You can choose any RGB value.
+color = (255, 0, 0)
 
-        # Filter out filenames that aren't numbers and convert them to integers
-        numbers = [int(f) for f in new_files if f.isdigit()]
-        print(numbers)
+# Create an image with the given color
+image = Image.new('RGB', (width, height), color)
 
-        # Return the highest number
-        return max(numbers)
-
-    except ValueError:
-        # This is raised if there are no numeric files and max() is called on an empty list
-        return None
-
-# Replace with the path to your directory
-directory_path = 'C:/Users/arunk/OneDrive/Documents/CMKL/the_palm_killer/test'
-
-highest_number = get_highest_numbered_file(directory_path)
-if highest_number is not None:
-    print(f"The highest numbered file is: {highest_number}")
-else:
-    print("No numeric file names found in the directory.")
+# Save the image as a .jpg file
+image.save('0.jpg')
