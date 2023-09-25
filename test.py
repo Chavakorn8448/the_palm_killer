17d2e1277dbed0b4d6b6de711a6662c32a72542a
Chavakorn8448 <arunkunarax.c.8448@gmail.com>
2023-09-25 14:09:54 +0700

    9/25

--- test.py
+++ test.py
@@ -1,36 +1,17 @@
-import cv2
-import numpy as np
 import picamera
-from picamera.array import PiRGBArray
 
-# Initialize the camera
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
+with picamera.PiCamera() as camera:
+    # Set camera resolution (you can adjust it based on your needs)
+    camera.resolution = (2592, 1944)  # Max resolution for v2.1 camera
 
-# Create a raw capture object to hold the frames
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-# Allow the camera to warm up
-import time
-time.sleep(0.1)
-
-# Capture the frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
+    # Optional: Provide a preview, which is useful for adjusting camera position
+    camera.start_preview()
     
-    # Use OpenCV to process the image (if you want to)
-    # For demonstration, we'll just convert the image to grayscale and show it
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Show the image
-    cv2.imshow("Frame", gray)
-    
-    # Clear the stream for the next frame
-    rawCapture.truncate(0)
-    
-    # If you press 'q', exit the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Allow the camera to warm-up
+    import time
+    time.sleep(2)
 
-cv2.destroyAllWindows()
+    # Capture the image
+    camera.capture('captured_image.jpg')
+    
+    camera.stop_preview()
