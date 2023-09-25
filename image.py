813ac0ee51aa6434c0c231fae9552d560ab178d2
Chavakorn8448 <arunkunarax.c.8448@gmail.com>
2023-09-25 22:17:15 +0700

    9/25

--- image.py
+++ image.py
@@ -3,13 +3,15 @@ from evdev import ecodes
 import os
 from PIL import Image
 from picamera2 import Picamera2, Preview
+import time
+from PIL import Image
 
-def take_pic(filename):
+def take_pic(path, filename):
     picam2 = Picamera2()
     picam2.start_preview(Preview.NULL)
     picam2.start()
     time.sleep(2)
-    metadata = picam2.capture_file("/home/traffy/the_palm_killer/" + filename + ".jpg")
+    metadata = picam2.capture_file("/home/traffy/the_palm_killer/" + path + "/" + str(filename) + ".jpg")
     picam2.close()
     print("captured")
 
@@ -35,22 +37,6 @@ def get_highest_numbered_file(path):
     except ValueError:
         # This is raised if there are no numeric files and max() is called on an empty list
         return None
-    
-def crop_image(image):
-    if image.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
-        # Calculate the middle region to crop
-        width, height = image.size
-        new_width, new_height = width // 2, height // 2
-        left = (width - new_width) / 2
-        top = (height - new_height) / 2
-        right = (width + new_width) / 2
-        bottom = (height + new_height) / 2
-
-        # Crop the middle of the image
-        cropped_image = image.crop((left, top, right, bottom))
-
-        # Save the cropped image to the destination folder
-        return cropped_image
 
 # Search for the device. In this example, we're looking for keyboards.
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
@@ -80,32 +66,12 @@ too_ripe = os.path.join(directory, 'too_ripe')
 
 if not os.path.exists(unripe):
     os.mkdir(unripe)
-    unripe_file = os.path.join(unripe, 'unripe.txt')
-    if not os.path.exists(unripe_file):
-        os.chdir(unripe)
-        with open('unripe.txt', 'w') as f:
-            pass
 if not os.path.exists(almost_ripe):
     os.mkdir(almost_ripe)
-    almost_ripe_file = os.path.join(almost_ripe, 'almost_ripe.txt')
-    if not os.path.exists(almost_ripe_file):
-        os.chdir(almost_ripe)
-        with open('almost_ripe.txt', 'w') as f:
-            pass
 if not os.path.exists(ripe):
     os.mkdir(ripe)
-    ripe_file = os.path.join(ripe, 'ripe.txt')
-    if not os.path.exists(ripe_file):
-        os.chdir(ripe)
-        with open('ripe.txt', 'w') as f:
-            pass
 if not os.path.exists(too_ripe):
     os.mkdir(too_ripe)
-    too_ripe_file = os.path.join(too_ripe, 'too_ripe.txt')
-    if not os.path.exists(too_ripe_file):
-        os.chdir(too_ripe)
-        with open('too_ripe.txt', 'w') as f:
-            pass
 
 unripe_last_num = get_highest_numbered_file(unripe)
 almost_ripe_last_num = get_highest_numbered_file(almost_ripe)
@@ -113,13 +79,13 @@ ripe_last_num = get_highest_numbered_file(ripe)
 too_ripe_last_num = get_highest_numbered_file(too_ripe)
 
 if unripe_last_num is None:
-    unripe_last_num = 1
+    unripe_last_num = 0
 if almost_ripe_last_num is None:
-    almost_ripe_last_num = 1
+    almost_ripe_last_num = 0
 if ripe_last_num is None:
-    ripe_last_num = 1
+    ripe_last_num = 0
 if too_ripe_last_num is None:
-    too_ripe_last_num = 1
+    too_ripe_last_num = 0
 
 # Infinite loop to check for key events
 for event in keyboard.read_loop():
@@ -127,20 +93,20 @@ for event in keyboard.read_loop():
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_1:
         print("'1' key pressed!")
         os.chdir(unripe)
-        take_pic(unripe_last_num)
         unripe_last_num += 1
+        take_pic("unripe", unripe_last_num)
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_2:
         print("'2' key pressed!")
         os.chdir(almost_ripe)
-        take_pic(almost_ripe_last_num)
         almost_ripe_last_num += 1
+        take_pic("almost_ripe", almost_ripe_last_num)
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_3:
         print("'3' key pressed!")
         os.chdir(ripe)
-        take_pic(ripe_last_num)
         ripe_last_num += 1
+        take_pic("ripe", ripe_last_num)
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_4:
         print("'4' key pressed!")
         os.chdir(too_ripe)
-        take_pic(too_ripe_last_num)
         too_ripe_last_num += 1
+        take_pic("too_ripe", too_ripe_last_num)
--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-import picamera
+import os
 
-with picamera.PiCamera() as camera:
-    # Set camera resolution (you can adjust it based on your needs)
-    camera.resolution = (2592, 1944)  # Max resolution for v2.1 camera
-
-    # Optional: Provide a preview, which is useful for adjusting camera position
-    camera.start_preview()
-    
-    # Allow the camera to warm-up
-    import time
-    time.sleep(2)
-
-    # Capture the image
-    camera.capture('captured_image.jpg')
-    
-    camera.stop_preview()
+directory = os.path.abspath("/traffy/the_palm_killer")
+print(directory)
