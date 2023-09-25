cb038f76d61eff3faac46b635ed6545f10a2b8e3
Chavakorn8448 <arunkunarax.c.8448@gmail.com>
2023-09-25 14:47:25 +0700

    9/25

--- image.py
+++ image.py
@@ -2,19 +2,16 @@ import evdev
 from evdev import ecodes
 import os
 from PIL import Image
+from picamera2 import Picamera2, Preview
 
-def create_image(number):
-    # Image size
-    width, height = 300, 300
-
-    # Color: in this case, it's red. You can choose any RGB value.
-    color = (255, 0, 0)
-
-    # Create an image with the given color
-    image = Image.new('RGB', (width, height), color)
-
-    # Save the image as a .jpg file
-    image.save(str(number) + '.jpg')
+def take_pic(filename):
+    picam2 = Picamera2()
+    picam2.start_preview(Preview.NULL)
+    picam2.start()
+    time.sleep(2)
+    metadata = picam2.capture_file("/home/traffy/the_palm_killer/" + filename + ".jpg")
+    picam2.close()
+    print("captured")
 
 # get last file number
 def get_only_filename(files):
@@ -130,20 +127,20 @@ for event in keyboard.read_loop():
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_1:
         print("'1' key pressed!")
         os.chdir(unripe)
-        create_image(unripe_last_num)
+        take_pic(unripe_last_num)
         unripe_last_num += 1
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_2:
         print("'2' key pressed!")
         os.chdir(almost_ripe)
-        create_image(almost_ripe_last_num)
+        take_pic(almost_ripe_last_num)
         almost_ripe_last_num += 1
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_3:
         print("'3' key pressed!")
         os.chdir(ripe)
-        create_image(ripe_last_num)
+        take_pic(ripe_last_num)
         ripe_last_num += 1
     if event.type == ecodes.EV_KEY and event.value == 1 and event.code == ecodes.KEY_4:
         print("'4' key pressed!")
         os.chdir(too_ripe)
-        create_image(too_ripe_last_num)
+        take_pic(too_ripe_last_num)
         too_ripe_last_num += 1
